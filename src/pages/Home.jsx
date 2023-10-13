81568cfaf2293acfcea4a0534100b509b67f6ea8
osamajavaid <iosamajavaid@gmail.com>
2023-10-13 20:30:40 +0500

    dummy content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,12 +6,13 @@ export default function Home() {
         <div className='flex flex-col gap-y-4 my-12'>
             <h2 className='text-2xl text-white font-semibold'>Libraries used in this stater</h2>
             <div className="grid grid-cols-5 w-full gap-4 text-LightGray">
-                <Card title="tailwindcss" />
-                <Card title="react-router-dom" />
                 <Card title="react-query" />
                 <Card title="axios" />
-                <Card title="dayjs" />
                 <Card title="vite-plugin-svgr" />
+                <Card title="react-router-dom" />
+                <Card title="Lazy Loading" />
+                <Card title="dayjs" />
+                <Card title="tailwindcss" />
                 <Card title="sass" />
             </div>
         </div>
---
+++ src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Login() {
+    return (
+        <div className='flex items-center justify-center'>
+            <h1 className='text-6xl text-red-600'>Login</h1>
+        </div>
+    )
+}
