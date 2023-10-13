5322ff4880c0db9b4ecc632cd05196d63d682580
osamajavaid <iosamajavaid@gmail.com>
2023-10-13 20:29:08 +0500

    routes added

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import Layout from '@layouts/Layout'
-import Home from '@pages/Home'
+import Login from '@pages/Login'
+import { routes_here } from './routes'
+import { Route, Routes } from 'react-router-dom'
+import ScrollTop from '@components/ScrollTop'
 
 export default function AppRoutes() {
+
+    const isAuthenticated = true; // bool, based on token availabilty
+
+    const renderRoute = (route, isAuthenticated) => {
+        if (route.isPrivate || isAuthenticated) {
+            return route.element;
+        }
+    };
+
     return (
-        <Layout>
-            <Home />
-        </Layout>
+        <Suspense fallback={<h1>Loading....</h1>}>
+            <React.Fragment>
+
+                <ScrollTop />
+                <Routes>
+                    {/* ================= All Routes ================ */}
+                    {routes_here.map((route) => (
+                        !isAuthenticated ?
+                            <Route path="/login" element={<Login />} />
+                            :
+                            <Route
+                                index
+                                key={route.id}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Suspense fallback={<h1>Loading....</h1>}>
+                                            {renderRoute(route, isAuthenticated)}
+                                        </Suspense>
+                                    </Layout>
+                                }
+                            />
+                    ))}
+                </Routes>
+
+
+            </React.Fragment>
+        </Suspense>
     )
 }
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,12 +1,27 @@
+import { lazy } from "react";
+
+
 //---------------------------
 // imports
 //---------------------------
+const Home = lazy(() => import("@pages/Home"));
+const Login = lazy(() => import("@pages/Login"));
 
 
 
 //---------------------------
 // exports
 //---------------------------
-export const Guest_Portal_Routes = [
+export const routes_here = [
+    {
+        path: "/",
+        element: <Home />,
+        isPrivate: false,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+        isPrivate: false,
+    },
     //routes in objects
 ];
