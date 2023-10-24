7c1dcfab1a17adaa92eec3b10eb7c17d83f1d2e0
osamajavaid <iosamajavaid@gmail.com>
2023-10-24 22:11:24 +0500

    new libs added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Card({ title }) {
     return (
-        <div className="flex items-center justify-center rounded-lg bg-DeepNightBlack hover:bg-EveningBlack h-20">
+        <div className="flex items-center text-2xl justify-center rounded-lg bg-DeepNightBlack hover:bg-EveningBlack h-20">
             <span>{title}</span>
         </div>
     )
